9b81091aa61214e7ac9cf4e60d6a63c3b269ef97
Christina Ly <cly168@vt.edu>
2016-09-02 16:14:37 -0400

    letter grade

---
+++ 24_letter_grade.js
@@ -0,0 +1,19 @@
+function lettergrade(score){
+	var grade = '';
+	if(score>=90){
+		grade = 'A';
+	}
+	else if (score>=80){
+		grade = 'B';
+	}
+	else if(score>=70){
+		grade = 'C';
+	}
+	else if(score>=60){
+		grade = 'D';
+	}
+	else{
+		grade = 'F';
+	}
+	console.log('Score: '+score+ ' Grade: '+grade);
+}
