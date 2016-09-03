db11f677233b380c79a6224a5d7e0a7f36b0ff5c
Christina Ly <cly168@vt.edu>
2016-09-03 00:25:56 -0400

    array rotate

---
+++ 40_array_rotate.js
@@ -0,0 +1,11 @@
+function rotateArr(arr,shiftBy){
+	for(var i = 0; i<shiftBy; i++){
+		for(var k = arr.length; k>0; k--){
+			arr[k] = arr[k-1];
+		}
+		arr[0] = arr[arr.length-1];
+		arr.pop();
+	}
+	return arr;
+}
+rotateArr()
