2678ec4a8d72f67db59c81a591c8ba9db771a810
Christina Ly <cly168@vt.edu>
2016-09-02 21:15:03 -0400

    finished

--- 33_gaming_fun(damentals).js
+++ 33_gaming_fun(damentals).js
@@ -23,4 +23,54 @@ function playStatsitics(){
 			max = roll;
 		}
 	}
+	console.log(min,max);
+}
+function playStatistics2(){
+	var min = 9;
+	var max = 0;
+	var sum = 0;
+	for(var i=0; i<8; i++){
+		var roll = rollOne();
+		if (roll<min){
+			min = roll;
+		}
+		if (roll>max){
+			max = roll;
+		}
+		sum += roll;
+	}
+	console.log(min,max,sum);
+}
+function playStatistics3(num){
+	var min = 9;
+	var max = 0;
+	var sum = 0;
+	for(var i=0; i<num; i++){
+		var roll = rollOne();
+		if (roll<min){
+			min = roll;
+		}
+		if (roll>max){
+			max = roll;
+		}
+		sum += roll;
+	}
+	console.log(min,max,sum);
+}
+function playStatistics4(num){
+	var min = 9;
+	var max = 0;
+	var sum = 0;
+	for(var i=0; i<num; i++){
+		var roll = rollOne();
+		if (roll<min){
+			min = roll;
+		}
+		if (roll>max){
+			max = roll;
+		}
+		sum += roll;
+	}
+	avg = sum / num;
+	console.log(min,max,avg);
 }
