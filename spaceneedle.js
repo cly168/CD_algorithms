5dd1ac8605d057f71efe4c77a2567bb59ef1dfa0
Christina Ly <cly168@vt.edu>
2016-09-03 23:54:05 -0400

    fixed space needle

--- spaceneedle.js
+++ spaceneedle.js
@@ -1,15 +1,15 @@
 for(var i=0; i<4; i++){
-	console.log("                      | |");
+	console.log("                      ||");
 }
-console.log("                   __/| |\\__");
+console.log("                   __/||\\__");
 
 var colon= ":::";
 var space= "   ";
 var leftwall = "__/";
 var rightwall="\\__";
-var cent = "| |";
-var lower = '/ \\';
-for(var i=0;i<4; i++){
+var cent = "||";
+var lower = '/\\';
+for(var i=0;i<3; i++){
 	var row=' '
 	for(var k=0; k <= 4-i; k++){
 		row+=space;
@@ -25,20 +25,65 @@ for(var i=0;i<4; i++){
 	row+=rightwall;
 	console.log(row);
 }
+row = '         ';
+cent = '\"';
+wall = "|";
+row+=wall;
+for(var i = 0; i<26; i++){
+	row+=cent;
+}
+row+=wall;
+console.log(row);
 leftwall = "_/";
 rightwall="\\_";
+space = "  "
 for(var i=0;i<4; i++){
-	var row=''
-	for(var k=0; k <=i; k++){
+	var row='        '
+	for(var k=0; k<=i; k++){
 		row+=space;
 	}
 	row+=rightwall;
-	for(var k=0; k<=11-(2*i); k++){
+	for(var k=0; k<11-(2*i); k++){
 		row+=lower;
 	}
 	row+=leftwall;
 	console.log(row);
 }
 for(var i=0; i<4; i++){
-	console.log("                      | |");
+	console.log("                      ||");
 }
+for(var i = 0; i<17; i++){;
+	console.log(row ="                   |%%||%%|");
+}
+console.log("                   __/||\\__");
+
+var colon= ":::";
+var space= "   ";
+var leftwall = "__/";
+var rightwall="\\__";
+var cent = "||";
+for(var i=0;i<3; i++){
+	var row=' '
+	for(var k=0; k <= 4-i; k++){
+		row+=space;
+	}
+	row+=leftwall;
+	for(var k=0; k<=i; k++){
+		row+=colon;
+	}
+	row+=cent;
+	for(var k=0; k<=i; k++){
+		row+=colon;
+	}
+	row+=rightwall;
+	console.log(row);
+}
+row = '         ';
+cent = '\"';
+wall = "|";
+row+=wall;
+for(var i = 0; i<26; i++){
+	row+=cent;
+}
+row+=wall;
+console.log(row)
